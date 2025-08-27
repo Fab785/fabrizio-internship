c9f3054481ae7ebe28e36fcd937be0172fa41083
Fabrizio <fabrizioterribile@gmail.com>
2025-08-27 04:11:01 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
 import ItemDetails from "./pages/ItemDetails";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <Nav />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/author/:authorId" element={<Author />} /> {/* Ensure you handle authorId in Author component */}
-            <Route path="/item-details/:itemId" element={<ItemDetails />} /> {/* Ensure you handle itemId in ItemDetails component */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/author/:authorId" element={<Author />} />
+          <Route path="/item-details/:itemId" element={<ItemDetails />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
+
+
+
+
+
